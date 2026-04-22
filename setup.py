6588e0f631631febec99ac731eb12b7c73f442fd
Quintin <quintins@uvic.ca>
2026-04-22 14:02:02 -0700

    bump python

--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ setup(
     keywords="climate meteorology cf conventions metadata",
     packages=find_packages(),
     version=".".join(str(d) for d in __version__),
+    python_requires=">=3.11,<3.14",
     url="http://www.pacificclimate.org/",
     author="Rod Glover",
     author_email="rglover@uvic.ca",
@@ -27,8 +28,10 @@ setup(
         Intended Audience :: Science/Research
         License :: OSI Approved :: GNU General Public License v3 (GPLv3)
         Operating System :: OS Independent
-        Programming Language :: Python :: 3.6
-        Programming Language :: Python :: 3.7
+        Programming Language :: Python :: 3
+        Programming Language :: Python :: 3.11
+        Programming Language :: Python :: 3.12
+        Programming Language :: Python :: 3.13
         Topic :: Internet
         Topic :: Scientific/Engineering
         Topic :: Database
